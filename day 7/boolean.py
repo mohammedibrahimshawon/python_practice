2502335674efb6510b4fe907d6e32d9e34492a48
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-05-31 17:55:36 +0600

    day7

---
+++ day 7/boolean.py
@@ -0,0 +1,27 @@
+# a = 12
+# b = 7
+
+# if a > b:
+#  print("Arash is older then Afif")
+
+# else :
+#  print("Arash is not older then Afif")
+
+
+print(bool("False"))
+print(bool("None"))
+print(bool("0"))
+print(bool("78"))
+print(bool("sd"))
+print(bool("d"))
+print(bool("7"))
+
+
+def myFunction() :
+  return False
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
