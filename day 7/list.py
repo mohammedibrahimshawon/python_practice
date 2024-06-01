45e666740b8c4de42be1bf2504f780a3ee56a9dd
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-06-01 16:49:45 +0600

    list

--- day 7/list.py
+++ day 7/list.py
@@ -1,5 +1,46 @@
-thislist = ["apple", "banana", "cherry"]
-if "melon" in thislist:
-  print("Yes, 'apple' is in the fruits list")
-else:
- print("melon is not in the fruit list")
+# thislist = ["apple", "banana", "cherry"]
+# if "melon" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+# else:
+#  print("melon is not in the fruit list")
+
+
+# Thislist = ["apple", "banana", "cherry"]
+# Thislist[3:4] = ["Arash"]
+
+# print(Thislist)
+
+
+
+# thislist = ["A","B","C"]
+# tropical = ["D","E","F"]
+
+# thislist.extend(tropical)
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# #pop
+# #remove
+# print(thislist)
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = [x for x in fruits if "a" in x]
+
+# print(newlist)
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if x != "apple"]
+
+# print(newlist)
+
+
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x.upper() for x in fruits]
+
+print(newlist)
