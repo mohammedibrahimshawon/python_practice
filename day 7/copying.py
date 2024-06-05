3bb6014fe3f8aac4a8b0e0eb2cf576d3b07fec54
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-06-05 16:46:34 +0600

    copyinginpyhton

---
+++ day 7/copying.py
@@ -0,0 +1,16 @@
+# Classsix = ["Arash","hk bbbvhjbjd","sakib","korim"]
+# Classseven =Classsix.copy()
+
+# print(Classseven)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+# print(mylist)
+
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
