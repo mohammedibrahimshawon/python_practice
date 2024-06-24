ea7884a894532da26adfb33792d4436c7397f91c
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-06-24 18:17:23 +0600

    dictionaryfunction

---
+++ day 10/dictionaly_modify.py
@@ -0,0 +1,8 @@
+thisdict = {
+  "name": "Arash",
+  "class": 7,
+  "age": 12
+}
+
+x = thisdict["class"]
+print(x)
---
+++ day 10/keys.py
@@ -0,0 +1,7 @@
+human  = {
+  "name": "Arash",
+  "class": 7,
+  "age": 12
+}
+human["eyecolor"] = "brown"
+print(human)
