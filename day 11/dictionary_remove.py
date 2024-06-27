101fda8f14176834992f11bf379ed4d217340a81
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-06-27 17:37:54 +0600

    dictionaryremoveitems

---
+++ day 11/dictionary_remove.py
@@ -0,0 +1,10 @@
+thisdict = {
+    "Name" : "ARASH",
+    "class" :7,
+    "Year" : 2011,
+    "School name" : "CMP SCHOOL AND COLLEGE",
+    "Birth place" : "CHATTOGRAM"
+}
+print(thisdict)
+thisdict.pop("School name")
+print(thisdict)
