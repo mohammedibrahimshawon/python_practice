026a6860d04bf25dfbd98c4cd60880201aff7f52
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-06-27 18:29:01 +0600

    loop

--- day 11/loop.py
+++ day 11/loop.py
@@ -1,3 +1,15 @@
+# thisdict = {
+#     "Grandfather's father name": "Abdul Korim",
+#     "Grandfather name": "Abul Basher",
+#     "Father name": "Iqbal Hossen",
+#     "My name": "Arash",
+#     "My son name": "NIks"
+#     }
+
+# for x in thisdict.values():
+#     print(x)
+
+
 thisdict = {
     "Grandfather's father name": "Abdul Korim",
     "Grandfather name": "Abul Basher",
@@ -6,5 +18,5 @@ thisdict = {
     "My son name": "NIks"
     }
 
-for x in thisdict.values():
+for x in thisdict.keys():
     print(x)
