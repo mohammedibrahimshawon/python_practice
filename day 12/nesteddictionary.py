c6493605e9f01f51fe02e77f45ab76c7a2f7326a
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-01 00:46:26 +0600

    nested dictionary

---
+++ day 12/nesteddictionary.py
@@ -0,0 +1,29 @@
+iqbalfamily = {
+    "child1" : {
+        "name" : "Iqbal Hossen",
+        "year" : 1981,
+        "gender" : "male"
+        
+    },
+
+     "child2" : {
+        "name" : "Rasel",
+        "year" : 196656565656565656565656594038,
+        "gender" : "male"
+        
+    }, "child3" : {
+        "name" : "Sumon",
+        "year" : 2578632753395,
+        "gender" : "male"
+        
+    }, "child4" : {
+        "name" : "sujon",
+        "year" : 198123445,
+        "gender" : "male"
+        
+    }
+
+}
+
+print(iqbalfamily)
+print(iqbalfamily["child3"] ["year"])
