880e678a1aea04f92383f057ccfd2908f80cba3e
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-04 17:20:12 +0600

    if-else statement

--- day 13/if-else.py
+++ day 13/if-else.py
@@ -1,11 +1,23 @@
-a = 3300
-b = 330
+# a = 3300
+# b = 330
 
-if b > a:
-     print("b is greater than a")
-elif a == b:
-  print("a and b are equal")     
+# if b > a:
+#      print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")     
+# else:
+#   print("a is greater than b")
+
+
+# a = 200
+# b = 200
+
+# print("a")if a > b else print("=") if a==b else print("b")
+
+a = 200
+b = 33000
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
 else:
-  print("a is greater than b")
-
-
+  print("error")  
