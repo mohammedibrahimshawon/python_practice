a5b54c2a20e3ac15aab6b5eaeba665fb2de9bbbe
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-04 18:03:47 +0600

    nestedloop

---
+++ day 13/nestedloop.py
@@ -0,0 +1,24 @@
+# x = 60
+
+# if x < 60:
+#   print("come back,")
+#   if x == 60:
+#       print("buy")
+# else:
+#     print("also buy.")
+
+
+# x = 41
+
+# if x > 10:
+#   print("Above ten,")
+#   if x > 20:
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.") 
+
+a = 33
+b = 200
+
+if b > a:
+  pass
