ee5aeb64d7bf3fb07f1717e951b5b94532fbddee
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-10 17:06:01 +0600

    for loop

---
+++ day 14/forloop.py
@@ -0,0 +1,11 @@
+# humans = ["Arash","Afifbot","Alifbot","noob","bot","ultra pro max bot","ultra pro max noob",]
+
+# for x in humans:
+#     print(x)
+#     if x == "noob":
+#       continue
+
+for x in range(3,19):
+    print(x)
+else:
+    print("Finally finished")    
