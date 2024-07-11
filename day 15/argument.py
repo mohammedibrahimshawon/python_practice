c4022b60e67be8326686969672d609d266a1df87
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-11 17:44:20 +0600

    argument

--- day 15/argument.py
+++ day 15/argument.py
@@ -1,6 +1,18 @@
-def my_function(fname):
-    print(fname + "  Iqbal")
+# def my_function(fname):
+#     print(fname + "  Iqbal")
 
-my_function("Arash")
-my_function("Noyon")
-my_function("Muna")
+# my_function("Arash")
+# my_function("Noyon")
+# my_function("Muna")
+
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Ibtesum", "Iqbal")
+# my_function("Iqbal", "Hossen")
+
+
+def my_function(child4, child3, child2, child1):
+  print("The youngest child is " + child4)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus", child4 = "Afif bot")
