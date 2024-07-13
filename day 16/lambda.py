aba2b8a61f282e587d3cd8447318a8fef1400703
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-13 18:01:15 +0600

    lambda.py

--- day 16/lambda.py
+++ day 16/lambda.py
@@ -1,3 +1,14 @@
 
-x = lambda a, b: a - b
-print(x(4, 2))
+# x = lambda a, b: a - b
+# print(x(4, 2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+myfourther = myfunc(4)
+
+print(mydoubler(11)) 
+print(mytripler(11))
+print(myfourther(11))
