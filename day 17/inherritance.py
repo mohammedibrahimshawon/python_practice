0ad51bbaa3852686e3ff23307c7a6945fac44e70
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-07-28 18:31:47 +0600

    parentchildinherritance

--- day 17/inherritance.py
+++ day 17/inherritance.py
@@ -1,12 +1,23 @@
-class person:
-    def __init__(self, fname, lname):
-     self.firstname = fname
-     self.lastname = lname
+# class person:
+#     def __init__(self, fname, lname):
+#      self.firstname = fname
+#      self.lastname = lname
 
-    def printname(self):
-      print(self.firstname, self.lastname)
+#     def printname(self):
+#       print(self.firstname, self.lastname)
 
 
-x =person("Ibtesum", "Iqbal")
-x.printname()        
+# x = person("Ibtesum", "Iqbal")
+# x.printname()        
     
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+
+x = Person("shown", "uddin")
+x.printname()
