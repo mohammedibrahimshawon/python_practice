e7f47a0540b42817430ffc4ae383432d3b666053
mohammedibrahimshawon <mshawonc3@gmail.com>
2024-08-08 23:30:55 +0600

    inheritancegame

---
+++ day 17/inheritancegame.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return f"{self.name} makes a sound."
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} barks."
+
+# Creating an instance of the Dog class
+dog = Dog("Buddy")
+print(dog.speak())
